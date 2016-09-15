0b756f6af5d1aaa07d007040f12ff6b66adc1ce0
madhudharmav <madhu.dharmav@gmail.com>
2016-09-15 16:10:07 +0000

    use only ceil till the discussion of trade-off between expansion and utilization rate is resolved

--- smalls/utilrate_format_driv_london_smalls.R
+++ smalls/utilrate_format_driv_london_smalls.R
@@ -6,34 +6,34 @@ table_10d_l_ceil$`North East`[table_10d_l_ceil$`North East`<2]<-2
 table_10d_l_ceil$`West (new)`[table_10d_l_ceil$`West (new)`<2]<-2
 table_10d_l_ceil$South[table_10d_l_ceil$South<2]<-2
 
-util_perc<-100*(rowMeans(table_10d_l,na.rm=TRUE)/rowMeans(table_10d_l_ceil,na.rm=TRUE))
-table_10d_l_ceil_old<-table_10d_l_ceil
-
-utilrate_range<-c(70,80)
-for(i1 in c(1:nrow(table_10d))){
-
-
-while (util_perc[i1]<utilrate_range[1] & !is.na(util_perc[i1])){
-  clusno<-which.min(table_10d_l[i1,]-floor(table_10d_l[i1,]))
-  if( !(names(table_10d_l_ceil)[clusno] %in% c("North East","South","West (new)"))){
-    if(table_10d_l_ceil[i1,clusno,with=FALSE]>1){
-      table_10d_l_ceil[i1,clusno:=.SD-1,.SDcols=(clusno),with=FALSE]
-     }
-  }else if(table_10d_l_ceil[i1,clusno,with=FALSE]>2){
-    table_10d_l_ceil[i1,clusno:=.SD-1,.SDcols=(clusno),with=FALSE]
-  }
-  if(identical(table_10d_l_ceil_old[i1,],table_10d_l_ceil[i1,])){break}
-  table_10d_l_ceil_old[i1,]<-table_10d_l_ceil[i1,]
-  util_perc[i1]<-100*(rowMeans(table_10d_l[i1,],na.rm=TRUE)/rowMeans(table_10d_l_ceil[i1,],na.rm=TRUE))
-} 
-  
- 
-while(util_perc[i1]>utilrate_range[2] & !is.na(util_perc[i1])){
-  clusno<-which.max(table_10d_l[i1,]-floor(table_10d_l[i1,]))
-  table_10d_l_ceil[i1,clusno:=.SD+1,.SDcols=(clusno),with=FALSE]
-  util_perc[i1]<-100*(rowMeans(table_10d_l[i1,],na.rm=TRUE)/rowMeans(table_10d_l_ceil[i1,],na.rm=TRUE))
-}
-}
+# util_perc<-100*(rowMeans(table_10d_l,na.rm=TRUE)/rowMeans(table_10d_l_ceil,na.rm=TRUE))
+# table_10d_l_ceil_old<-table_10d_l_ceil
+# 
+# utilrate_range<-c(70,80)
+# for(i1 in c(1:nrow(table_10d))){
+# 
+# 
+# while (util_perc[i1]<utilrate_range[1] & !is.na(util_perc[i1])){
+#   clusno<-which.min(table_10d_l[i1,]-floor(table_10d_l[i1,]))
+#   if( !(names(table_10d_l_ceil)[clusno] %in% c("North East","South","West (new)"))){
+#     if(table_10d_l_ceil[i1,clusno,with=FALSE]>1){
+#       table_10d_l_ceil[i1,clusno:=.SD-1,.SDcols=(clusno),with=FALSE]
+#      }
+#   }else if(table_10d_l_ceil[i1,clusno,with=FALSE]>2){
+#     table_10d_l_ceil[i1,clusno:=.SD-1,.SDcols=(clusno),with=FALSE]
+#   }
+#   if(identical(table_10d_l_ceil_old[i1,],table_10d_l_ceil[i1,])){break}
+#   table_10d_l_ceil_old[i1,]<-table_10d_l_ceil[i1,]
+#   util_perc[i1]<-100*(rowMeans(table_10d_l[i1,],na.rm=TRUE)/rowMeans(table_10d_l_ceil[i1,],na.rm=TRUE))
+# } 
+#   
+#  
+# while(util_perc[i1]>utilrate_range[2] & !is.na(util_perc[i1])){
+#   clusno<-which.max(table_10d_l[i1,]-floor(table_10d_l[i1,]))
+#   table_10d_l_ceil[i1,clusno:=.SD+1,.SDcols=(clusno),with=FALSE]
+#   util_perc[i1]<-100*(rowMeans(table_10d_l[i1,],na.rm=TRUE)/rowMeans(table_10d_l_ceil[i1,],na.rm=TRUE))
+# }
+# }
 return(table_10d_l_ceil)
 }
 
